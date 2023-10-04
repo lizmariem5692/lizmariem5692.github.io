60302d774bab1111802ee4029cba5d57fb1ec700
lizmariem5692 <146863534+lizmariem5692@users.noreply.github.com>
2023-10-04 15:00:59 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = "Lizmarie Mendoza"; // update the part BETWEEN the "quotes"
+let userName = "lizmariem5692"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
